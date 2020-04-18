8f92e6500652e073b9c5a69261d924c2a9d688d5
Orion Kindel <cakekindel@gmail.com>
2020-04-18 15:20:16 -0700

    add screenshot gen script to improve ergonomics

--- assets/readme-samples/src/10-000-feet.snippet.ts
+++ assets/readme-samples/src/10-000-feet.snippet.ts
@@ -1,14 +1,17 @@
-// for a value `val`,
-let output = strike(val,
-    match(/* if it matches `x`      */, /* then do `a` */),
+// pseudo-code example:
+let output = strike(
+    val, // for a value `val`,
+    match(/* if it matches `x` */, /* then do `a` */),
     match(/* else if it matches `y` */, /* then do `b` */),
-    // ... any number of other `match` arms
-    otherwise(/* else do this */),
+    /* ...any number of other `match` arms */
+    otherwise(/* do this */),
 );
 
-// e.g. `message` will be set to 'big number!'
+// simple example:
 let message = strike(23,
     match(10, 'ten'),
     match(n => n > 10, 'big number!'),
     otherwise('some other number')
 );
+
+assert(message, 'big number!');
