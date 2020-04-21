8c5ec1b2107429f5fa85abff71236950dc9fdab6
Orion Kindel <cakekindel@gmail.com>
2020-04-20 17:53:09 -0700

    docs: fox docs for AnyExecutor

--- src/internal/match-execution/types/any-executor.ts
+++ src/internal/match-execution/types/any-executor.ts
@@ -2,6 +2,12 @@ import {DefaultExecutor} from './default-executor';
 import {MatchExecutor} from './match-executor';
 import {UnwrapExecutor} from './unwrap-executor';
 
+/**
+ * @description
+ * Accepts the output of `match`, `unwrap`, or `otherwise`.
+ *
+ * @internal
+ */
 export type AnyExecutor<TIn, TOut> =
     | MatchExecutor<TIn, TOut>
     | DefaultExecutor<TIn, TOut>
