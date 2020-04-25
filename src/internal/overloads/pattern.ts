fdc3db3550d221a8f1e1ba08767f3079fb9b68e5
Orion Kindel <cakekindel@gmail.com>
2020-04-25 14:51:08 -0700

    feat!: remove unwrap from pattern & strike overloads

--- src/internal/overloads/pattern.ts
+++ src/internal/overloads/pattern.ts
@@ -1,5 +1,4 @@
 import {DefaultExecutor, MatchExecutor} from '../match-execution/types';
-import {UnwrapExecutor} from '../match-execution/types/unwrap-executor';
 
 /* eslint-disable no-undef */
 
@@ -157,25 +156,25 @@ export interface PatternNonExhaustiveSigs {
 export interface PatternExhaustiveSigs {
     <TIn, TOut_0, TOut_1>(
         arm_0: MatchExecutor<TIn, TOut_0>,
-        otherwise: DefaultExecutor<TIn, TOut_1> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_1>
     ): (val: TIn) => TOut_0 | TOut_1;
     <TIn, TOut_0, TOut_1, TOut_2>(
         arm_0: MatchExecutor<TIn, TOut_0>,
         arm_1: MatchExecutor<TIn, TOut_1>,
-        otherwise: DefaultExecutor<TIn, TOut_2> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_2>
     ): (val: TIn) => TOut_0 | TOut_1 | TOut_2;
     <TIn, TOut_0, TOut_1, TOut_2, TOut_3>(
         arm_0: MatchExecutor<TIn, TOut_0>,
         arm_1: MatchExecutor<TIn, TOut_1>,
         arm_2: MatchExecutor<TIn, TOut_2>,
-        otherwise: DefaultExecutor<TIn> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_3>
     ): (val: TIn) => TOut_0 | TOut_1 | TOut_2 | TOut_3;
     <TIn, TOut_0, TOut_1, TOut_2, TOut_3, TOut_4>(
         arm_0: MatchExecutor<TIn, TOut_0>,
         arm_1: MatchExecutor<TIn, TOut_1>,
         arm_2: MatchExecutor<TIn, TOut_2>,
         arm_3: MatchExecutor<TIn, TOut_3>,
-        otherwise: DefaultExecutor<TIn> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_4>
     ): (val: TIn) => TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4;
     <TIn, TOut_0, TOut_1, TOut_2, TOut_3, TOut_4, TOut_5>(
         arm_0: MatchExecutor<TIn, TOut_0>,
@@ -183,7 +182,7 @@ export interface PatternExhaustiveSigs {
         arm_2: MatchExecutor<TIn, TOut_2>,
         arm_3: MatchExecutor<TIn, TOut_3>,
         arm_4: MatchExecutor<TIn, TOut_4>,
-        otherwise: DefaultExecutor<TIn> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_5>
     ): (val: TIn) => TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4 | TOut_5;
     <TIn, TOut_0, TOut_1, TOut_2, TOut_3, TOut_4, TOut_5, TOut_6>(
         arm_0: MatchExecutor<TIn, TOut_0>,
@@ -192,7 +191,7 @@ export interface PatternExhaustiveSigs {
         arm_3: MatchExecutor<TIn, TOut_3>,
         arm_4: MatchExecutor<TIn, TOut_4>,
         arm_5: MatchExecutor<TIn, TOut_5>,
-        otherwise: DefaultExecutor<TIn> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_6>
     ): (
         val: TIn
     ) => TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4 | TOut_5 | TOut_6;
@@ -204,7 +203,7 @@ export interface PatternExhaustiveSigs {
         arm_4: MatchExecutor<TIn, TOut_4>,
         arm_5: MatchExecutor<TIn, TOut_5>,
         arm_6: MatchExecutor<TIn, TOut_6>,
-        otherwise: DefaultExecutor<TIn> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_7>
     ): (
         val: TIn
     ) => TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4 | TOut_5 | TOut_6 | TOut_7;
@@ -228,7 +227,7 @@ export interface PatternExhaustiveSigs {
         arm_5: MatchExecutor<TIn, TOut_5>,
         arm_6: MatchExecutor<TIn, TOut_6>,
         arm_7: MatchExecutor<TIn, TOut_7>,
-        otherwise: DefaultExecutor<TIn> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_8>
     ): (
         val: TIn
     ) =>
@@ -263,7 +262,7 @@ export interface PatternExhaustiveSigs {
         arm_6: MatchExecutor<TIn, TOut_6>,
         arm_7: MatchExecutor<TIn, TOut_7>,
         arm_8: MatchExecutor<TIn, TOut_8>,
-        otherwise: DefaultExecutor<TIn> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_9>
     ): (
         val: TIn
     ) =>
@@ -301,7 +300,7 @@ export interface PatternExhaustiveSigs {
         arm_7: MatchExecutor<TIn, TOut_7>,
         arm_8: MatchExecutor<TIn, TOut_8>,
         arm_9: MatchExecutor<TIn, TOut_9>,
-        otherwise: DefaultExecutor<TIn, TOut_10> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_10>
     ): (
         val: TIn
     ) =>
--- src/internal/overloads/strike.ts
+++ src/internal/overloads/strike.ts
@@ -3,7 +3,6 @@ import {
     DefaultExecutor,
     MatchExecutor,
 } from '../match-execution/types';
-import {UnwrapExecutor} from '../match-execution/types/unwrap-executor';
 
 /* eslint-disable no-undef */
 
@@ -160,35 +159,23 @@ export interface NonExhaustiveStrikeSigs {
  * @internal
  */
 export interface ExhaustiveStrikeSigs {
-    <TIn, TOut_0>(
-        val: TIn,
-        arm_0: MatchExecutor<typeof val, TOut_0>,
-        otherwise: DefaultExecutor<TIn, TOut_0> | UnwrapExecutor
-    ): TOut_0;
     <TIn, TOut_0, TOut_1>(
         val: TIn,
         arm_0: MatchExecutor<typeof val, TOut_0>,
-        arm_1: MatchExecutor<typeof val, TOut_1>,
-        otherwise: DefaultExecutor<TIn, TOut_0 | TOut_1> | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_1>
     ): TOut_0 | TOut_1;
-    <TIn, TOut_0, TOut_1, TOut_2, TOut_3>(
+    <TIn, TOut_0, TOut_1, TOut_2>(
         val: TIn,
         arm_0: MatchExecutor<typeof val, TOut_0>,
         arm_1: MatchExecutor<typeof val, TOut_1>,
-        arm_2: MatchExecutor<typeof val, TOut_2>,
-        otherwise:
-            | DefaultExecutor<TIn, TOut_0 | TOut_1 | TOut_2>
-            | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_2>
     ): TOut_0 | TOut_1 | TOut_2;
     <TIn, TOut_0, TOut_1, TOut_2, TOut_3>(
         val: TIn,
         arm_0: MatchExecutor<typeof val, TOut_0>,
         arm_1: MatchExecutor<typeof val, TOut_1>,
         arm_2: MatchExecutor<typeof val, TOut_2>,
-        arm_3: MatchExecutor<typeof val, TOut_3>,
-        otherwise:
-            | DefaultExecutor<TIn, TOut_0 | TOut_1 | TOut_2 | TOut_3>
-            | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_3>
     ): TOut_0 | TOut_1 | TOut_2 | TOut_3;
     <TIn, TOut_0, TOut_1, TOut_2, TOut_3, TOut_4>(
         val: TIn,
@@ -196,10 +183,7 @@ export interface ExhaustiveStrikeSigs {
         arm_1: MatchExecutor<typeof val, TOut_1>,
         arm_2: MatchExecutor<typeof val, TOut_2>,
         arm_3: MatchExecutor<typeof val, TOut_3>,
-        arm_4: MatchExecutor<typeof val, TOut_4>,
-        otherwise:
-            | DefaultExecutor<TIn, TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4>
-            | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_4>
     ): TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4;
     <TIn, TOut_0, TOut_1, TOut_2, TOut_3, TOut_4, TOut_5>(
         val: TIn,
@@ -208,13 +192,7 @@ export interface ExhaustiveStrikeSigs {
         arm_2: MatchExecutor<typeof val, TOut_2>,
         arm_3: MatchExecutor<typeof val, TOut_3>,
         arm_4: MatchExecutor<typeof val, TOut_4>,
-        arm_5: MatchExecutor<typeof val, TOut_5>,
-        otherwise:
-            | DefaultExecutor<
-                  TIn,
-                  TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4 | TOut_5
-              >
-            | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_5>
     ): TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4 | TOut_5;
     <TIn, TOut_0, TOut_1, TOut_2, TOut_3, TOut_4, TOut_5, TOut_6>(
         val: TIn,
@@ -224,13 +202,7 @@ export interface ExhaustiveStrikeSigs {
         arm_3: MatchExecutor<typeof val, TOut_3>,
         arm_4: MatchExecutor<typeof val, TOut_4>,
         arm_5: MatchExecutor<typeof val, TOut_5>,
-        arm_6: MatchExecutor<typeof val, TOut_6>,
-        otherwise:
-            | DefaultExecutor<
-                  TIn,
-                  TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4 | TOut_5 | TOut_6
-              >
-            | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_6>
     ): TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4 | TOut_5 | TOut_6;
     <TIn, TOut_0, TOut_1, TOut_2, TOut_3, TOut_4, TOut_5, TOut_6, TOut_7>(
         val: TIn,
@@ -241,20 +213,7 @@ export interface ExhaustiveStrikeSigs {
         arm_4: MatchExecutor<typeof val, TOut_4>,
         arm_5: MatchExecutor<typeof val, TOut_5>,
         arm_6: MatchExecutor<typeof val, TOut_6>,
-        arm_7: MatchExecutor<typeof val, TOut_7>,
-        otherwise:
-            | DefaultExecutor<
-                  TIn,
-                  | TOut_0
-                  | TOut_1
-                  | TOut_2
-                  | TOut_3
-                  | TOut_4
-                  | TOut_5
-                  | TOut_6
-                  | TOut_7
-              >
-            | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_7>
     ): TOut_0 | TOut_1 | TOut_2 | TOut_3 | TOut_4 | TOut_5 | TOut_6 | TOut_7;
     <
         TIn,
@@ -277,21 +236,7 @@ export interface ExhaustiveStrikeSigs {
         arm_5: MatchExecutor<typeof val, TOut_5>,
         arm_6: MatchExecutor<typeof val, TOut_6>,
         arm_7: MatchExecutor<typeof val, TOut_7>,
-        arm_8: MatchExecutor<typeof val, TOut_8>,
-        otherwise:
-            | DefaultExecutor<
-                  TIn,
-                  | TOut_0
-                  | TOut_1
-                  | TOut_2
-                  | TOut_3
-                  | TOut_4
-                  | TOut_5
-                  | TOut_6
-                  | TOut_7
-                  | TOut_8
-              >
-            | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_8>
     ):
         | TOut_0
         | TOut_1
@@ -325,22 +270,7 @@ export interface ExhaustiveStrikeSigs {
         arm_6: MatchExecutor<typeof val, TOut_6>,
         arm_7: MatchExecutor<typeof val, TOut_7>,
         arm_8: MatchExecutor<typeof val, TOut_8>,
-        arm_9: MatchExecutor<typeof val, TOut_9>,
-        otherwise:
-            | DefaultExecutor<
-                  TIn,
-                  | TOut_0
-                  | TOut_1
-                  | TOut_2
-                  | TOut_3
-                  | TOut_4
-                  | TOut_5
-                  | TOut_6
-                  | TOut_7
-                  | TOut_8
-                  | TOut_9
-              >
-            | UnwrapExecutor
+        otherwise: DefaultExecutor<TIn, TOut_9>
     ):
         | TOut_0
         | TOut_1
@@ -352,6 +282,44 @@ export interface ExhaustiveStrikeSigs {
         | TOut_7
         | TOut_8
         | TOut_9;
+    <
+        TIn,
+        TOut_0,
+        TOut_1,
+        TOut_2,
+        TOut_3,
+        TOut_4,
+        TOut_5,
+        TOut_6,
+        TOut_7,
+        TOut_8,
+        TOut_9,
+        TOut_10
+    >(
+        val: TIn,
+        arm_0: MatchExecutor<typeof val, TOut_0>,
+        arm_1: MatchExecutor<typeof val, TOut_1>,
+        arm_2: MatchExecutor<typeof val, TOut_2>,
+        arm_3: MatchExecutor<typeof val, TOut_3>,
+        arm_4: MatchExecutor<typeof val, TOut_4>,
+        arm_5: MatchExecutor<typeof val, TOut_5>,
+        arm_6: MatchExecutor<typeof val, TOut_6>,
+        arm_7: MatchExecutor<typeof val, TOut_7>,
+        arm_8: MatchExecutor<typeof val, TOut_8>,
+        arm_9: MatchExecutor<typeof val, TOut_9>,
+        otherwise: DefaultExecutor<TIn, TOut_10>
+    ):
+        | TOut_0
+        | TOut_1
+        | TOut_2
+        | TOut_3
+        | TOut_4
+        | TOut_5
+        | TOut_6
+        | TOut_7
+        | TOut_8
+        | TOut_9
+        | TOut_10;
 }
 
 /**
